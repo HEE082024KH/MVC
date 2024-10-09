a77af7cada9b3b1d43be251f992ee3d6f97a520f
hee082024kh <hee082024@gmail.com>
2024-10-09 10:22:18 +0200

    Fixed errors

--- Program.cs
+++ Program.cs
@@ -4,21 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Controller controller = new Controller();
-
         SongList song = new SongList();
         View view = new View();
-        Console.WriteLine("Type u to create/update, type o to output");
-        string? input = Console.ReadLine();
-        switch (input)
-        {
-            case u:
-                controller.UpdateSong();
-                break;
-            case o:
-            // Output list
-            default:
-                break;
-        }
+        Controller controller = new Controller(song, view);
+
+        // Console.WriteLine("Type u to create/update, type o to output");
+        // string? input = Console.ReadLine();
+        // switch (input)
+        // {
+        //     case u:
+        //         controller.UpdateSong();
+        //         break;
+        //     case o:
+        //     // Output list
+        //     default:
+        //         break;
+        // }
     }
 }
